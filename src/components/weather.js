1f2f5fa867f6e4dda672ca4ddeb0d8b6be0a9706
mberke27 <mberkemelis@gmail.com>
2024-09-19 23:06:52 -0700

    added popup

--- src/components/weather.js
+++ src/components/weather.js
@@ -7,6 +7,7 @@ function Weather() {
   const [location, setLocation] = useState("");
   const [isloading, setIsLoading] = useState(false);
   const [bgColor, setBgColor] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
 
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=47d2c9a35c63b7400c4dbaeb61cb53c1&units=metric`;
 
@@ -39,9 +40,10 @@ function Weather() {
       }
     } catch (error) {
       setLocation("");
+      setShowPopup(true);
       // setData({});
       console.error(error);
-      alert("Please enter a valid location ");
+      // alert("Please enter a valid location ");
     } finally {
       setIsLoading(false);
     }
@@ -58,90 +60,106 @@ function Weather() {
         <h1 className="title mt-2 font-black text-5xl text-stone-50 text-center">
           WeatherNow{" "}
         </h1>
-      </div>
 
-      <div className="search">
-        <form onSubmit={fetchLocation}>
-          <input
-            type="text"
-            placeholder="Enter Location..."
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-          <button className="bg-sunset hover:bg-sunset-dark text-white py-3 px-6 rounded-full shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl ml-3s">
-            Find
-          </button>
-        </form>
-      </div>
-
-      <div className="container">
-        <div className="top">
-          <a href={`https://en.wikipedia.org/wiki/${location}`}>
-            {data.sys ? (
-              <p className="city-name">
-                {" "}
-                {data.name}, {data.sys.country}
-              </p>
-            ) : null}
-          </a>
-          <div className="temp">
-            {data.main ? (
-              <h1>
-                {" "}
-                {Math.round(data.main.temp)} <span>&deg;C</span>
-              </h1>
-            ) : null}
-            <div className="description">
-              {data.weather ? (
-                <p>
-                  {data.weather[0].description.charAt(0).toUpperCase() +
-                    data.weather[0].description.slice(1)}
-                  <span>
-                    <img
-                      className="logo"
-                      src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
-                      alt="weather icon"
-                    />
-                  </span>
-                </p>
-              ) : null}
-            </div>
-          </div>
+        <div className="search">
+          <form onSubmit={fetchLocation}>
+            <input
+              type="text"
+              placeholder="Enter Location..."
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            <button className="bg-sunset hover:bg-sunset-dark text-white py-4 px-6 rounded-full shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-xl ml-3s">
+              Find
+            </button>
+          </form>
         </div>
 
-        {data.name !== undefined && (
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? (
-                <p className="bold">
+        <div className="container">
+          <div className="top">
+            <a href={`https://en.wikipedia.org/wiki/${location}`}>
+              {data.sys ? (
+                <p className="city-name">
                   {" "}
-                  {Math.round(data.main.feels_like)} &deg; C
+                  {data.name}, {data.sys.country}
                 </p>
               ) : null}
-              <p2>Feels Like</p2>
-            </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-              <p2>Humidity</p2>
-            </div>
-            <div className="windy">
-              {" "}
-              {data.wind ? (
-                <p className="bold">{Math.round(data.wind.speed)} km/h</p>
+            </a>
+            <div className="temp">
+              {data.main ? (
+                <h1>
+                  {" "}
+                  {Math.round(data.main.temp)} <span>&deg;C</span>
+                </h1>
               ) : null}
-              <p2>Wind</p2>
+              <div className="description">
+                {data.weather ? (
+                  <p>
+                    {data.weather[0].description.charAt(0).toUpperCase() +
+                      data.weather[0].description.slice(1)}
+                    <span>
+                      <img
+                        className="logo"
+                        src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+                        alt="weather icon"
+                      />
+                    </span>
+                  </p>
+                ) : null}
+              </div>
+            </div>
+          </div>
+
+          {data.name !== undefined && (
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold">
+                    {" "}
+                    {Math.round(data.main.feels_like)} &deg; C
+                  </p>
+                ) : null}
+                <p2>Feels Like</p2>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p2>Humidity</p2>
+              </div>
+              <div className="windy">
+                {" "}
+                {data.wind ? (
+                  <p className="bold">{Math.round(data.wind.speed)} km/h</p>
+                ) : null}
+                <p2>Wind</p2>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {showPopup && (
+          <div className="popup">
+            <div className="popup-content">
+              <h2>Invalid Location</h2>
+              <button
+                style={{ width: "100px", padding: "10px", marginTop: "10px" }}
+                onClick={() => setShowPopup(false)}
+              >
+                Close
+              </button>
             </div>
           </div>
         )}
-      </div>
 
-      <div>
-        <a
-          className="footer fixed bottom-3 antialiased w-full text-center py-2 font-source-sans-pro font-helvetica font-sans text-white text-opacity-20 cursor-default text-s uppercase tracking-wider hover:text-white"
-          href="https://mberkekaradayi.com/"
-        >
-          &copy;Mehmet Berke Karadayi
-        </a>
+        <div className="flex items-center justify-center fixed bottom-0 w-full py-2 bg-transparent">
+          <a
+            className="footer antialiased text-center font-source-sans-pro font-helvetica font-sans text-white text-opacity-20 cursor-default text-s uppercase tracking-wider hover:text-white"
+            href="https://mberkekaradayi.com/"
+          >
+            &copy; Mehmet Berke Karadayi
+          </a>
+        </div>
       </div>
     </div>
   );
